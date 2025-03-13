10d9d58ef3d1dd716508e8b9c614fa4ba1c17203
Vasileios-Georgios Zantalis <vzntalis@classter.com>
2025-03-13 08:48:16 +0200

    checking for sender and receiver before creating a message

--- ShadowNetBackend/Features/Communications/CreateMessage/CreateMessageCommandHandler.cs
+++ ShadowNetBackend/Features/Communications/CreateMessage/CreateMessageCommandHandler.cs
@@ -1,20 +1,27 @@
-﻿namespace ShadowNetBackend.Features.Communications.CreateMessage;
+﻿using ShadowNetBackend.Features.Agents.GetByIdAgent;
+
+namespace ShadowNetBackend.Features.Communications.CreateMessage;
 
 public class CreateMessageCommandHandler : IRequestHandler<CreateMessageCommand, Guid>
 {
     private readonly ApplicationDbContext _dbContext;
+    private readonly ISender _sender;
     private readonly ICryptographyService _cryptographyService;
     private readonly ICacheService _cache;
 
-    public CreateMessageCommandHandler(ApplicationDbContext dbContext, ICryptographyService cryptographyService, ICacheService cache)
+    public CreateMessageCommandHandler(ApplicationDbContext dbContext, ISender sender, ICryptographyService cryptographyService, ICacheService cache)
     {
         _dbContext = dbContext;
+        _sender = sender;
         _cryptographyService = cryptographyService;
         _cache = cache;
     }
 
     public async Task<Guid> Handle(CreateMessageCommand request, CancellationToken cancellationToken)
     {
+        await _sender.Send(new GetByIdAgentQuery(request.SenderId), cancellationToken);
+        await _sender.Send(new GetByIdAgentQuery(request.ReceiverId), cancellationToken);
+
         var message = new Message
         {
             SenderId = request.SenderId,
